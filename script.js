4c551e363055ff086ff6406e80179c7f464ce823
nenshallom <sshallom543@gmail.com>
2025-03-12 15:18:55 +0100

    project section

--- script.js
+++ script.js
@@ -105,12 +105,12 @@ const activePortfolio = () => {
 }
 
 arrowRight.addEventListener('click', () => {
-    if (index < 4) {
+    if (index < 1) {
         index++;
         arrowLeft.classList.remove('disabled');
     }
     else {
-        index = 5;
+        index = 2;
         arrowRight.classList.add('disabled');
     }
 
